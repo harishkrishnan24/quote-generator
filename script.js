d4d34a20e920c8facde30774aa16d4e1b0863a5d
Harish krishnan <harishkrishnan1993@gmail.com>
2021-04-03 20:41:20 +0530

    Add new quote and twitter open

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-// Get Quote From API
-async function getQuote() {
-	const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-	const apiUrl =
-		"http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
-	try {
-		const response = await fetch(proxyUrl + apiUrl);
-		const data = await response.json();
-		console.log(data);
-	} catch (error) {
-		getQuote();
-		console.log("Whoops, no quote", error);
-	}
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
-// On Load
-getQuote();
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+getQuotes();
